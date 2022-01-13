236a723f765fde519ff90de440c399eea1fd95be
Thiago Amaral <Thiago>
2022-01-13 15:36:48 -0300

    meu primeiro commit

---
+++ lampada.js
@@ -0,0 +1,45 @@
+const turnOnOff = document.getElementById('turnOnOff');
+const broken = document.getElementById('broken');
+const lamp = document.getElementById('lamp');
+
+
+function isLampBroken() {
+    return lamp.src.indexOf('quebrada') > -1
+}
+
+function lampOn() {
+    if (!isLampBroken()) {
+
+        lamp.src = './img/ligada.jpg';
+    }
+
+}
+
+
+function lampOff() {
+    if (!isLampBroken()) {
+
+        lamp.src = './img/desligada.jpg';
+    }
+}
+
+function lampBroken() {
+    lamp.src = './img/quebrada.jpg';
+}
+
+function lampOnOff() {
+    if (turnOnOff.textContent == 'Ligar') {
+        lampOn();
+        turnOnOff.textContent = 'Desligar';
+    } else {
+        lampOff();
+        turnOnOff.textContent = 'Ligar';
+    }
+}
+
+
+turnOnOff.addEventListener('click', lampOnOff);
+
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseout', lampOff);
+lamp.addEventListener('dblclick', lampBroken);
